162f23e2f0c49739ace05f2598bf1fd10ec809d9
david-hp-0726 <xche653@emory.edu>
2023-07-24 10:35:57 +0800

    Update ProjectItem.js

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 function ProjectItem({ name, language, framework_library, github_link, image}) {
     return (
-        <button onClick={() => window.open(github_link)}>
-            <div className="projectItem">
-                <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
-                <h1>{name}</h1>
-                <h4>({language}, {framework_library})</h4>
-            </div>
-        </button>
+        <div className="projectItem" onClick={() => window.open(github_link)}>
+            <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
+            <h1>{name}</h1>
+            <h4>({language}, {framework_library})</h4>
+        </div>
     )
 }
 
