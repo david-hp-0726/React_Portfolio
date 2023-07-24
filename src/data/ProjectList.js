73492485b5d50857b1835b6b033c973629e0f1b2
david-hp-0726 <xche653@emory.edu>
2023-07-24 09:27:44 +0800

    Update ProjectList.js

--- src/data/ProjectList.js
+++ src/data/ProjectList.js
@@ -9,28 +9,28 @@ const ProjectList = [
         name: "Markdown Article Blog", 
         language: "JavaScript",
         framework_library: "React",
-        github_link: "/",
+        github_link: "https://github.com/david-hp-0726/Markdown_Blog",
         image: Image_Blog
     },
     {
         name: "Task Manager",
         language: "Python",
         framework_library: "Django",
-        github_link: "/",
+        github_link: "https://github.com/david-hp-0726/To_Do_App",
         image: Image_Todoapp
     },
     {
         name: "Study Covid-19 with ML Model",
         language: "Python",
         framework_library: "XGBoost",
-        github_link: "/",
+        github_link: "https://github.com/david-hp-0726/Python_Data_Science_Project",
         image: Image_Xgboost
     },
     {
         name: "Analysis of GCloud Translation API", 
         language: "Python",
         framework_library: "Google Cloud",
-        github_link: "/",
+        github_link: "https://github.com/david-hp-0726/Gcloud_Translation_API_Project",
         image: Image_Translation
     }
 ]
